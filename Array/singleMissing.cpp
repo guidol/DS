3061eb654916775044b0a4ce5bf7f6fe16f571f6
almarz@gmail.com <almarz@gmail.com>
2019-08-20 12:28:06 +0900

    Array with C++

--- Array/singleMissing.cpp
+++ Array/singleMissing.cpp
@@ -17,6 +17,44 @@ class Array{
         A = new int[sz];
         length = 0;
     }
-        
-
+    ~Array(){
+        delete []A;
+    }
+    void Display();
+    void Insert(int index, int x);
+    int Delete(int index);
 };
+ void Array::Display(){
+     for(int i=0; i < length; i++)
+        cout<< A[i] << " ";
+        cout<<endl;     
+ }
+ void Array::Insert(int index, int x){
+     //if( index >= 0 && index < length)
+     for(int i=length-1 ; i >=index ; i--)
+            A[i+1]=A[i];
+     A[index] = x;
+     length++;
+ }
+ int Array::Delete(int index){
+     int x = 0;
+     if(index >=0 && index < length){
+         x = A[index];
+         for(int i = index; i < length-1 ; i++)
+             A[i] = A[i+1];
+         length--;         
+     }
+     return x;
+ }
+
+ int main(){
+        Array arr(10);
+        arr.Insert(0,5);
+        arr.Insert(1,6);
+        arr.Insert(2,9);
+        arr.Display();
+        cout<<arr.Delete(0)<<endl;
+        arr.Display();
+    
+    return 0;
+ }
