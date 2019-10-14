f087158b149674578663312f6d889fb961ed1391
almarz@gmail.com <almarz@gmail.com>
2019-10-14 16:23:42 +0900

    adding Selection Sort

--- Sorting/Sorting.cpp
+++ Sorting/Sorting.cpp
@@ -5,6 +5,7 @@ class sorting{
        public:
             void BubbleSort(int a[], int n);
             void InsertionSort(int a[], int n);
+            void SelectionSort(int a[], int n);
             void swap(int *x, int *y);
 };
 void sorting::swap(int *x, int *y){
@@ -42,12 +43,23 @@ void sorting::InsertionSort(int a[], int n){
         a[j+1] = x;
     }
 }
+void sorting::SelectionSort(int a[],int n){
+    int i,j,k=0;
+    for(i = 0; i < n-1 ; i++){
+        for(j=k=i; j < n ; j++){
+            if(a[j] < a[k])
+                k=j;
+        }
+        swap(&a[i],&a[k]);
+    }
+}
 int main(){
     int A[10] = {3,7,9,10,6,5,12,4,11,2};
 
     sorting srt;
     //srt.BubbleSort(A,10);
-    srt.InsertionSort(A,10);
+    //srt.InsertionSort(A,10);
+    srt.SelectionSort(A,10);
     for(int i=0 ; i < 10 ; i++)
         cout<<" "<<A[i];
     cout<<endl;
